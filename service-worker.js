3807399b89ed4e9a09b7a52d61134a73b69679cb
Stanley Hon <stahon@microsoft.com>
2020-01-23 10:58:25 -0800

    Adding some code.

--- service-worker.js
+++ service-worker.js
@@ -11,3 +11,22 @@ self.addEventListener('fetch', function(event) {
       )
     );
   });
+
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/offline.html'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
